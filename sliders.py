693dd8f9bbbd109691fbcc7381f0557027191251
Pulkit Mittal <2pulkit2@gmail.com>
2021-10-03 01:06:08 +0530

    add sliders

--- sliders.py
+++ sliders.py
@@ -4,25 +4,32 @@ screen=Screen()
 
 class Sliders:
     def __init__(self):
-        self.segments=[]
-        for i in range(3):
-            new_obj=Turtle("square")            
-            new_obj.color("white")
-            new_obj.penup()
-            self.segments.append(new_obj)
+        self.segments_one=[]
+        self.segments_two=[]
+        
 
     def default_pos(self):
         
         x_cor=screen.window_width()/2
-        y_cor=30
-        for i in range(3):
-            new_obj_one=Sliders()
-            new_obj_one.segments[i].goto(x=-x_cor+30,y=y_cor)
-            y_cor-=20
+        y_cor=90
+        for i in range(14):
+            if i<7:
+                new_obj=Turtle("square")
+                new_obj.color("white")
+                new_obj.penup()
+                new_obj.goto(x=-x_cor+30,y=y_cor)
+                y_cor-=20
+                self.segments_one.append(new_obj)
+            
+            else:
+                new_obj=Turtle("square")
+                new_obj.color("white")
+                new_obj.penup()
+                new_obj.goto(x=x_cor-30,y=y_cor)
+                y_cor+=20
+                self.segments_one.append(new_obj)
+                # reversing the list to make both the lists homogeneous
+                self.segments_two=self.segments_two[::-1]
+
     
-        x_cor=screen.window_width()/2
-        y_cor=30
-        for i in range(3):
-            new_obj_two=Sliders()
-            new_obj_two.segments[i].goto(x=x_cor-30,y=y_cor)
-            y_cor-=20
+        
